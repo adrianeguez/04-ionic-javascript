0c2259d347c3f3edceb0fa589ec560701ed67332
Adrian Eguez <eadepto@hotmail.com>
2018-06-15 16:46:42 -0500

    passwords iguales

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, ToastController} from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -11,12 +11,33 @@ export class HomePage {
   nombre = "Hola";
   password = "";
   passwordConfirmacion = "";
+  duracionToast = 3000;
+  posicionToast = 'top';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public toastCtrl: ToastController) {
   }
 
   validarFormulario(formulario: NgForm) {
     console.log(formulario);
+    if (formulario.controls.password.value === // form
+      this.passwordConfirmacion) { // ngModel
+      // OK!
+    } else {
+      this.mostrarToast('Los passwords deben de ser iguales');
+      this.password = "";
+      this.passwordConfirmacion = "";
+    }
+  }
+
+  mostrarToast(mensaje: string) {
+    const toast = this.toastCtrl
+      .create({
+        message: mensaje,
+        duration: this.duracionToast, // 3000
+        position: this.posicionToast // 'top'
+      });
+    toast.present();
   }
 
 }
