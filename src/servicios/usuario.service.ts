4714c4837873e2465cb442e41f662d5a34bb4500
Adrian Eguez <eadepto@hotmail.com>
2018-06-15 17:23:41 -0500

    emitir cambio nombre usuario

--- src/servicios/usuario.service.ts
+++ src/servicios/usuario.service.ts
@@ -1,16 +1,25 @@
-import {Injectable} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 
 @Injectable()
 export class UsuarioService {
   nombreUsuario = 'Adrian';
 
-  // static sumarDosNumeros(numeroUno: number, numeroDos: number) {
-  //   return numeroUno + numeroDos;
-  // }
+  cambioNombreUsuario: EventEmitter<string> = new EventEmitter();
 
   sumarDosNumeros(numeroUno: number, numeroDos: number) {
     return numeroUno + numeroDos;
   }
+
+  emitirCambioNombreUsuario(nombreUsuario: string) {
+    this.nombreUsuario = nombreUsuario;
+    this.cambioNombreUsuario.emit(nombreUsuario);
+  }
+
+
+  // static sumarDosNumeros(numeroUno: number, numeroDos: number) {
+  //   return numeroUno + numeroDos;
+  // }
+
 }
 
 // UsuarioService.sumarDosNumeros()
